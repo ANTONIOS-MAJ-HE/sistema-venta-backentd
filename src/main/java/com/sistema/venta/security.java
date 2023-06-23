c1a90edc33661336a3e3023b48a3f9df1e30dd8f
LuisAngelG <luis.gastelu@tecsup.edu.pe>
2023-06-23 09:04:47 -0500

    2123

---
+++ src/main/java/com/sistema/venta/security.java
@@ -0,0 +1,7 @@
+package com.sistema.venta;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+public class security extends WebSecurityConfiguration{
+
+}
